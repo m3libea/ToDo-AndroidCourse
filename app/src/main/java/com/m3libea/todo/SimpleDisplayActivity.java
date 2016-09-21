c7f514d456035e914693f8d5f71eace29c914495
Rocio Romero <rociorommor@gmail.com>
2016-09-20 19:35:37 -0700

    First commit

---
+++ app/src/main/java/com/m3libea/todo/SimpleDisplayActivity.java
@@ -0,0 +1,13 @@
+package com.m3libea.todo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SimpleDisplayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_simple_displey);
+    }
+}
